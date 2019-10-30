98afed1688a63131bba6a5cdba8accd66ebca510
hedongfeng <hedongfeng@qingting.fm>
2019-10-30 16:58:11 +0800

    m

--- train.py
+++ train.py
@@ -533,6 +533,9 @@ class Trainer:
                 data_list = pickle.load(f)
         else:
             data_list = self.load_data()
+        labels_list = list(map(lambda x: x[5], data_list))
+        label_sum = np.sum(labels_list, axis=0).tolist()
+        print("label_sum", label_sum)
         if self.debug_mode is False:
             self.seed_everything()
             data_list = random.sample(data_list, int(len(data_list) * self.use_ratio))
@@ -705,7 +708,7 @@ if __name__ == "__main__":
     trainer = Trainer(epochs=10, batch_size=16, seed=1, use_ratio=0.01, split_ratio=0.8, lr=3e-4, weight_decay=0.0001,
                       optimizer="adam", lr_schedule="", warmup_steps=2000, use_grad_clip=True, max_grad=1.0,
                       use_apex=False, output_model=False, emb_dir="emb/", data_dir=data_dir,
-                      model_save_dir="model/", debug_mode=False, use_seq_emb=True, use_seq_cnt=False, embedding_size=10,
+                      model_save_dir="model/", debug_mode=True, use_seq_emb=True, use_seq_cnt=False, embedding_size=10,
                       is_shallow_dropout=True, dropout_shallow=(0.5, 0.5), deep_layers=(32, 32), is_deep_dropout=True,
                       dropout_deep=(0.5, 0.5, 0.5), deep_layers_activation='relu', is_batch_norm=False, use_plain_emb=True,
                       use_lstm=False, use_tcn=True, use_avg=False, use_att=True, seq_emb_size=64, seq_hidden_size=32,
