1334d899414793b8cc649c92e749282991018e5f
hedongfeng <hedongfeng@qingting.fm>
2019-10-31 16:49:37 +0800

    m

--- train.py
+++ train.py
@@ -716,7 +716,7 @@ if __name__ == "__main__":
     else:
         data_dir = "/root/ctr/"
     # weight_decay 可能会造成权重过小 nan
-    trainer = Trainer(epochs=10, batch_size=16, seed=1, use_ratio=0.1, split_ratio=0.8, lr=3e-4, weight_decay=0.000,
+    trainer = Trainer(epochs=10, batch_size=16, seed=1, use_ratio=0.01, split_ratio=0.8, lr=3e-4, weight_decay=0.000,
                       optimizer="adam", lr_schedule="", warmup_steps=2000, use_grad_clip=True, max_grad=1.0,
                       use_apex=False, output_model=True, emb_dir="emb/", data_dir=data_dir,
                       model_save_dir="model/", debug_mode=False, use_seq_emb=True, use_seq_cnt=True, embedding_size=10,
